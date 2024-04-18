87d958d34341941ecc9e7a1aa72b2f1ddc8a9d6d
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-04-18 21:23:34 +0100

    Added rate structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::{error::Error, io, process};
+
+struct Rate {
+    no: String,
+    effectiveDate: String,
+    mid: f64
+}
+
+struct ExchangeRate {
+    table: char,
+    currency: String,
+    code: String,
+    rates: Vec<Rate>
+}
+
+fn example() -> Result<(), Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::Reader::from_path("transactions.csv")?;
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
 fn main() {
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
     println!("Hello, world!");
 }
