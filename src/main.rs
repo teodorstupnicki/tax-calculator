8268a496ce0e5cc91e4b29e1063469383691e897
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-04-19 10:35:06 +0100

    Added API calls and response deserialization

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::{error::Error, io, process};
+use reqwest;
+use time;
 
+#[derive(Debug, serde::Deserialize)]
 struct Rate {
     no: String,
     effectiveDate: String,
     mid: f64
 }
 
+#[derive(Debug, serde::Deserialize)]
 struct ExchangeRate {
     table: char,
     currency: String,
@@ -13,19 +17,51 @@ struct ExchangeRate {
     rates: Vec<Rate>
 }
 
-fn example() -> Result<(), Box<dyn Error>> {
+#[derive(Debug, serde::Deserialize)]
+struct Row<'a> {
+    date: &'a str,
+    operation: &'a str,
+    sent_amount: f64,
+    sent_currency: &'a str,
+    received_amount: f64,
+    received_currency: &'a str,
+    fee_amount: f64,
+    fee_currency: &'a str,
+}
+
+static API_URL: &str = "http://api.nbp.pl/api/exchangerates/rates/a/eur/";
+
+async fn example() -> Result<(), Box<dyn Error>> {
     // Build the CSV reader and iterate over each record.
-    let mut rdr = csv::Reader::from_path("transactions.csv")?;
+    let mut rdr = csv::ReaderBuilder::new()
+        .delimiter(b';').from_path("transactions_clean.csv")?;
     for result in rdr.records() {
         let record = result?;
-        println!("{:?}", record);
+        let row: Row = record.deserialize(None)?;
+        println!("{:?}", row);
+
+        let request_url = format!("{API_URL}{0}", row.date);
+        println!("{}", request_url);
+
+        let response = reqwest::get(&request_url).await?;
+        if response.status().is_success() {
+            println!("Success API call!");
+        } else {
+            let format = format_description!("[year]-[month]-[day]");
+            let date = Date::parse(row.date, &format)?;
+            println!("{}", date);
+        }
+        let object = response.json::<ExchangeRate>().await?;
+        println!("{:?}", object);
+        // println!("{:?}", response);
     }
     Ok(())
 }
-fn main() {
-    if let Err(err) = example() {
+
+#[tokio::main]
+async fn main() {
+    if let Err(err) = example().await {
         println!("error running example: {}", err);
         process::exit(1);
     }
-    println!("Hello, world!");
 }
