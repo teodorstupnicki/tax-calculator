5d3a14e4cbc431e3b07192ae4bffbf9167e64ba4
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-04-20 00:08:28 +0100

    Full functionality implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{error::Error, io, process};
 use reqwest;
-use time;
+use time::Date;
+use time::macros::format_description;
 
 #[derive(Debug, serde::Deserialize)]
 struct Rate {
@@ -32,6 +33,10 @@ struct Row<'a> {
 static API_URL: &str = "http://api.nbp.pl/api/exchangerates/rates/a/eur/";
 
 async fn example() -> Result<(), Box<dyn Error>> {
+    let mut buy_net: f64 = 0.0;
+    let mut buy_net_pln: f64 = 0.0;
+    let mut sell_net: f64 = 0.0;
+    let mut sell_net_pln: f64 = 0.0;
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::ReaderBuilder::new()
         .delimiter(b';').from_path("transactions_clean.csv")?;
@@ -39,22 +44,46 @@ async fn example() -> Result<(), Box<dyn Error>> {
         let record = result?;
         let row: Row = record.deserialize(None)?;
         println!("{:?}", row);
-
-        let request_url = format!("{API_URL}{0}", row.date);
+        
+        let format = format_description!("[year]-[month]-[day]");
+        let mut date = Date::parse(row.date, &format)?;
+        date = date.previous_day().unwrap();
+        
+        let mut request_url = format!("{API_URL}{0}", date);
         println!("{}", request_url);
 
-        let response = reqwest::get(&request_url).await?;
-        if response.status().is_success() {
-            println!("Success API call!");
-        } else {
-            let format = format_description!("[year]-[month]-[day]");
-            let date = Date::parse(row.date, &format)?;
-            println!("{}", date);
+        let mut response = reqwest::get(&request_url).await?;
+        while response.status().is_client_error() {
+            println!("Not a working day.");
+            date = date.previous_day().unwrap(); 
+            request_url = format!("{API_URL}{0}", date);
+            println!("{}", request_url);
+            response = reqwest::get(&request_url).await?;
         }
+        
         let object = response.json::<ExchangeRate>().await?;
         println!("{:?}", object);
+        let rate = object.rates.first().unwrap();
+        let exchange_rate = rate.mid;
         // println!("{:?}", response);
+        if row.operation == "Buy" {
+            buy_net += row.sent_amount;
+            let pln_amount = row.sent_amount * exchange_rate;
+            buy_net_pln += pln_amount;
+        } else if row.operation == "Sell" {
+            sell_net += row.received_amount;
+            let pln_amount = row.received_amount * exchange_rate;
+            sell_net_pln += pln_amount;
+        }
     }
+    let net = sell_net - buy_net;
+    let net_pln = sell_net_pln - buy_net_pln;
+    println!("Sum of EUR spent on crypto: {:.2}", buy_net);
+    println!("Sum of PLN spent on crypto: {:.2}", buy_net_pln);
+    println!("Sum of EUR sold crypto for: {:.2}", sell_net);
+    println!("Sum of PLN sold crypto for: {:.2}", sell_net_pln);
+    println!("Net amount in EUR: {:.2}", net);
+    println!("Net amount in PLN: {:.2}", net_pln);
     Ok(())
 }
 
