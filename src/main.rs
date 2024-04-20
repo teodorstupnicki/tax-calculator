8c04ea31ad24745b1cf7afa3ccce086b20a6cc5a
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-04-20 23:19:32 +0100

    Added progress bar

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{error::Error, io, process};
 use reqwest;
 use time::Date;
 use time::macros::format_description;
+use indicatif::ProgressBar;
 
 #[derive(Debug, serde::Deserialize)]
 struct Rate {
@@ -40,32 +41,35 @@ async fn example() -> Result<(), Box<dyn Error>> {
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::ReaderBuilder::new()
         .delimiter(b';').from_path("transactions_clean.csv")?;
+    let count = rdr.records().count() as u64;
+    rdr = csv::ReaderBuilder::new()
+        .delimiter(b';').from_path("transactions_clean.csv")?;
+    let pb = ProgressBar::new(count);
     for result in rdr.records() {
         let record = result?;
         let row: Row = record.deserialize(None)?;
-        println!("{:?}", row);
+        // println!("{:?}", row);
         
         let format = format_description!("[year]-[month]-[day]");
         let mut date = Date::parse(row.date, &format)?;
         date = date.previous_day().unwrap();
         
         let mut request_url = format!("{API_URL}{0}", date);
-        println!("{}", request_url);
+        // println!("{}", request_url);
 
         let mut response = reqwest::get(&request_url).await?;
         while response.status().is_client_error() {
-            println!("Not a working day.");
+            // println!("Not a working day.");
             date = date.previous_day().unwrap(); 
             request_url = format!("{API_URL}{0}", date);
-            println!("{}", request_url);
+            // println!("{}", request_url);
             response = reqwest::get(&request_url).await?;
         }
         
         let object = response.json::<ExchangeRate>().await?;
-        println!("{:?}", object);
+        // println!("{:?}", object);
         let rate = object.rates.first().unwrap();
         let exchange_rate = rate.mid;
-        // println!("{:?}", response);
         if row.operation == "Buy" {
             buy_net += row.sent_amount;
             let pln_amount = row.sent_amount * exchange_rate;
@@ -75,7 +79,9 @@ async fn example() -> Result<(), Box<dyn Error>> {
             let pln_amount = row.received_amount * exchange_rate;
             sell_net_pln += pln_amount;
         }
+        pb.inc(1);
     }
+    pb.finish_and_clear();
     let net = sell_net - buy_net;
     let net_pln = sell_net_pln - buy_net_pln;
     println!("Sum of EUR spent on crypto: {:.2}", buy_net);
