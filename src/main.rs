110623d52af2b1f69f1f54709bb2eedaf8f2fa40
teodorstupnicki <teodor.stupnicki@gmail.com>
2024-06-20 22:08:59 +0200

    Added test module for main

--- src/main.rs
+++ src/main.rs
@@ -133,3 +133,14 @@ async fn main() {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(1, 2), 3);
+    }
+}
